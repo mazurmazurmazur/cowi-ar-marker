b3d2a6b8222f9bf149d9f72d30d891a005466aab
mazurmazurmazur <mazurjestboski@mazurjestboskis-MacBook-Pro.local>
2020-02-04 15:44:37 +0100

    json fetched, auto generated pictures react with names assigned in json

--- js/main.js
+++ js/main.js
@@ -1,15 +1,28 @@
-let link =
-  "https://cmv.cowi.com/geoserver/cmv/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=cmv:dk_wfs_annotation_naithani&maxFeatures=50&outputFormat=application%2Fjson";
+let link = "https://my.api.mockaroo.com/users.json?key=a99e7550";
 
 function fetchContact(x) {
-  console.log("link dziala poprawne: " + x);
-  fetch(x)
+  fetch(link)
     .then(res => res.json())
     .then(fillHome);
 }
 
 function fillHome(json) {
   console.log(json);
+
+  document.getElementById("scene");
+  json.forEach(el => {
+    let newMarker = document.createElement("a-marker");
+    let newText = document.createElement("a-text");
+
+    newMarker.setAttribute("type", "barcode");
+    newMarker.setAttribute("value", el.id);
+    newText.setAttribute("value", el.first_name);
+    newText.setAttribute("rotation", "-90 0 0");
+    newText.setAttribute("geometry", "primitive:plane");
+    newText.setAttribute("color", "red");
+    newMarker.appendChild(newText);
+    scene.appendChild(newMarker);
+  });
   // let acf = json[0].acf;
   // let infographics = Object.entries(acf.infographics);
   // //selecting json area
