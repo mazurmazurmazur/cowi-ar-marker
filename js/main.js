03e792dab35dc72e8aa194b4297d7a53043f560e
mazurmazurmazur <mazurjestboski@mazurjestboskis-MacBook-Pro.local>
2020-02-05 11:47:38 +0100

    showcase of two json files, offline and online generating markers

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
-let link = "https://my.api.mockaroo.com/users.json?key=a99e7550";
+let link1 = "https://my.api.mockaroo.com/users.json?key=a99e7550";
+let link2 = "./json/geo.json";
 
 function fetchContact(x) {
-  fetch(link)
+  fetch(x)
     .then(res => res.json())
     .then(fillHome);
 }
@@ -9,58 +10,30 @@ function fetchContact(x) {
 function fillHome(json) {
   console.log(json);
 
+  let jsonFE = json.features ? json.features : json;
+
   document.getElementById("scene");
-  json.forEach(el => {
+  jsonFE.forEach(el => {
     let newMarker = document.createElement("a-marker");
     let newText = document.createElement("a-text");
 
+    if (el.id) {
+      newMarker.setAttribute("value", el.id);
+      newText.setAttribute("value", el.first_name);
+    } else {
+      newMarker.setAttribute("value", properties.id);
+      newText.setAttribute("value", el.id);
+    }
+
     newMarker.setAttribute("type", "barcode");
-    newMarker.setAttribute("value", el.id);
-    newText.setAttribute("value", el.first_name);
+
     newText.setAttribute("rotation", "-90 0 0");
+    newText.setAttribute("z-offset", "1");
     newText.setAttribute("geometry", "primitive:plane");
     newText.setAttribute("color", "red");
     newMarker.appendChild(newText);
     scene.appendChild(newMarker);
   });
-  // let acf = json[0].acf;
-  // let infographics = Object.entries(acf.infographics);
-  // //selecting json area
-  // let topTitleJSON = json[0].acf.main_title;
-  // let buttonNextJSON = acf.next_page;
-
-  // let homeDescJSON = json[0].acf.main_description;
-  // //assigning json content to DOM
-  // topTitle.innerHTML = topTitleJSON;
-  // homeDesc.innerHTML = homeDescJSON;
-  // buttonNext.innerHTML = buttonNextJSON;
-  // let aboutContent = document.getElementById("about-content");
-
-  // infographics.forEach(infographic => {
-  //   //creating DOM Elements
-  //   let newGridItem = document.createElement("div");
-  //   let newIGTitle = document.createElement("p");
-  //   let newIG = document.createElement("img");
-
-  //   //inserting elements inside containerDiv
-  //   newGridItem.appendChild(newIGTitle);
-  //   newGridItem.appendChild(newIG);
-
-  //   //assigning classnames
-  //   newGridItem.classList.add("grid-item");
-  //   newGridItem.classList.add("double");
-
-  //   newIGTitle.classList.add("sectionTitle");
-  //   newIGTitle.classList.add("subtitle");
-  //   newIGTitle.classList.add("centred");
-
-  //   //assigning content to DOM Elements
-  //   if (infographic[1].image) {
-  //     newIG.src = infographic[1].image;
-  //     newIGTitle.innerHTML = infographic[1].title;
-  //     aboutContent.appendChild(newGridItem);
-  //   }
-  // });
 }
 
-fetchContact(link);
+fetchContact(link1);
