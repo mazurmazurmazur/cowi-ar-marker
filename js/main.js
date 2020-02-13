1fcb09d05d155a8ed51ede2174569293ec69a09c
mazurmazurmazur <mazurjestboski@mazurjestboskis-MacBook-Pro.local>
2020-02-13 11:19:29 +0100

    simplifying app

--- js/main.js
+++ js/main.js
@@ -14,22 +14,18 @@ function fillHome(json) {
   document.getElementById("scene");
   jsonFE.forEach(el => {
     let newMarker = document.createElement("a-marker");
+    let newBox = document.createElement("a-entity");
     let newText = document.createElement("a-text");
 
-    if (el.first_name) {
-      newMarker.setAttribute("value", el.id);
-      newText.setAttribute("value", el.first_name);
-    } else {
-      newMarker.setAttribute("value", el.properties.id);
-      newText.setAttribute("value", el.id);
-    }
+    newMarker.setAttribute("value", el.properties.id);
+    newText.setAttribute("value", el.properties.id);
 
     newMarker.setAttribute("type", "barcode");
 
     newText.setAttribute("rotation", "-90 0 0");
     newText.setAttribute("z-offset", "1");
-    newText.setAttribute("geometry", "primitive:plane");
     newText.setAttribute("color", "red");
+
     newMarker.appendChild(newText);
     scene.appendChild(newMarker);
   });
